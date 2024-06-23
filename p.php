2e374cf73be29cd745822e501e1cc71bf2e5c3bf
AhmedAli154 <152240307+AhmedAli154@users.noreply.github.com>
2024-06-23 15:00:48 +0300

    Add files via upload

---
+++ p.php
@@ -0,0 +1,44 @@
+
+<!-- ------------task 1---------------------- -->
+
+<!-- <?php
+
+phpinfo();
+?> -->
+<!-----------------end----------------------- -->
+
+<!-- ------------task 2---------------------- -->
+<!-- 
+<?php
+define('site_name','ahmedPhp');
+
+echo site_name;
+
+?>  -->
+
+<!-----------------end----------------------- -->
+
+<!-- ------------task 3---------------------- -->
+<!-- <?php
+$brother_age=10;
+switch($brother_age){
+    case ($brother_age < "5"):
+        echo "Stay at home";
+    break ;    
+    case ($brother_age == "5"):
+        echo "Go to Kindergarden";
+        break;     
+        case in_array ($brother_age , range(6,12)):
+        $brother_age -=6 ;
+        echo "Go to Grade =======>  $brother_age" ;
+}
+?> -->
+
+
+
+
+
+
+
+
+<!-----------------end----------------------- -->
